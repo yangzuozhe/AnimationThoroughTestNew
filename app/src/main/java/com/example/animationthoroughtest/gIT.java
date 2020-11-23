485bf0ad3893844ed461641b5ccb1d64c4b8b625
yangzuozhe <yangzuozhe@taqu.cn>
2020-11-23 14:08:21 +0800

    hello

---
+++ app/src/main/java/com/example/animationthoroughtest/gIT.java
@@ -0,0 +1,10 @@
+package com.example.animationthoroughtest;
+
+/**
+ * 类描述
+ *
+ * @author HB.yangzuozhe
+ * @date 2020-11-23
+ */
+public class gIT {
+}
---
+++ app/src/main/java/com/example/animationthoroughtest/widget/ASDASD.java
@@ -0,0 +1,11 @@
+package com.example.animationthoroughtest.widget;
+
+/**
+ * 类描述
+ *
+ * @author HB.yangzuozhe
+ * @date 2020-11-23
+ */
+public class ASDASD
+{
+}
